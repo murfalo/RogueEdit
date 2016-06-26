615f45f702e992a2214e29cb5c152876f30a0513
Murphy Angelo <murphycangelo@gmail.com>
2016-06-26 16:15:25 -0400

    Add strings for interfacing with UI

--- strings.h
+++ strings.h
@@ -22,8 +22,24 @@ namespace Strings
 
     // Strings for UI
     static const QString toolbarObjectName = "mainToolBar";
+    static const QString navigationObjectName = "navigationWidget";
     static const QString characterMenuObjectName = "menuCharacter";
     static const QString loadCharacterDropdownObjectName = "dropdownLoad_Character";
+    static const QString nameEditObjectName = "lineEditName";
+    static const QString difficultyEditObjectName = "comboBoxDifficultyEdit";
+    static const QString raceEditObjectName = "comboBoxRaceEdit";
+    static const QString classEditObjectName = "comboBoxClassEdit";
+    static const QString variantEditObjectName = "comboBoxVariantEdit";
+    static const QString uniformEditObjectName = "comboBoxUniformEdit";
+    static const QString augmentEditObjectName = "comboBoxAugmentEdit";
+    static const QString vitalityEditObjectName = "spinBoxVitVal";
+    static const QString dexterityEditObjectName = "spinBoxDexVal";
+    static const QString magicEditObjectName = "spinBoxMagVal";
+    static const QString strengthEditObjectName = "spinBoxStrVal";
+    static const QString techEditObjectName = "spinBoxTecVal";
+    static const QString faithEditObjectName = "spinBoxFaiVal";
+    static const QString characterLevelObjectName = "spinBoxLevelVal";
+    static const QString allegianceLevelObjectName = "spinBoxAllegianceLevelVal";
     static const QString saveCharacterActionText = "Save Character";
 
     /* Strings for parsing PlayerPrefs.txt.
@@ -37,12 +53,32 @@ namespace Strings
     static const std::string intSpecifier = "System.Int32";
     static const std::string stringSpecifier = "System.String";
     static const std::string nameSpecifier = "name";
+    static const std::string difficultySpecifier = "prof";
+    static const std::string raceSpecifier = "race";
+    static const std::string classSpecifier = "class";     // WARNING: Does not have an associated value in PlayerPrefs.txt
+    static const std::string primaryStat1 = "trait0";
+    static const std::string primaryStat2 = "trait1";
+    static const std::string variantSpecifier = "variant";
+    static const std::string uniformSpecifier = "uniform";
+    static const std::string augmentSpecifier = "augment";
+    static const std::string vitalitySpecifier = "pStat0";
+    static const std::string dexteritySpecifier = "pStat1";
+    static const std::string magicSpecifier = "pStat2";
+    static const std::string strengthSpecifier = "pStat3";
+    static const std::string techSpecifier = "pStat4";
+    static const std::string faithSpecifier = "pStat5";
+    static const std::string characterLevelSpecifier = "level";
+    static const std::string allegianceLevelSpecifier = "aq";
 
+
+    // String arrays for determining settings
+    static const int NUM_DIFFICULTIES = 2;
     static const int NUM_RACES = 24;
     static const int NUM_STATS = 6;       // NUM_STATS is used instead of NUM_CLASSES b/c class depends only on primary stats of a character
     static const int NUM_VARIANTS = 3;
     static const int NUM_UNIFORMS = 24;
     static const int NUM_AUGMENTS = 24;
+    static const std::string difficulties[NUM_DIFFICULTIES] = { "Normal", "Ironman"};
     static const std::string races[NUM_RACES] = { "Wanderer", "Royalite", "Centurion", "Illuminate", "Shlaami", "Fishfolk", "Gekko",
                                                   "Nomad", "Deathrazor", "Hiveling", "Ancient", "Lightsworn", "Drifter", "Goblin",
                                                   "Swampfolk", "Tiki", "Titan", "Trogon", "Scaled", "Florbgon", "Oompa", "Wizened",
@@ -64,4 +100,5 @@ namespace Strings
                                                         "Chamcham Hat", "Demon Horns", "Forsaker Mask", "Shroom Hat", "Halo", "Creator Mask",
                                                         "Rebellion Headpiece", "Gas Mask" };
 }
+
 #endif
