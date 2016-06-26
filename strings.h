7dc1502cb3817f77644cb900cf1987416f9d19b1
Murphy Angelo <murphycangelo@gmail.com>
2016-06-26 10:35:22 -0400

    Create string arrays for variant, race, augment, and uniform

--- strings.h
+++ strings.h
@@ -38,13 +38,30 @@ namespace Strings
     static const std::string stringSpecifier = "System.String";
     static const std::string nameSpecifier = "name";
 
-    static const int NUM_CLASSES = 15;   // Number of character classes
-    static const int NUM_STATS = 6;
-    static const std::string classes[NUM_STATS][NUM_STATS] = { { NULL, "Enforcer", "Gunner", "Machinist", "Darkmage", "Aethermage" },
-                                                               { "Enforcer", NULL, "Blademaster", "Dragoon", "Spellblade", "Aetherknight" },
-                                                               { "Gunner", "Blademaster", NULL, "Bounty Hunter", "Gunmage", "Commander" },
-                                                               { "Machinist", "Dragoon", "Bounty Hunter", NULL, "Datamancer", "Alchemist" },
-                                                               { "Darkmage", "Spellblade", "Gunmage", "Datamancer", NULL, "Arcanist" },
-                                                               { "Aethermage", "Aetherknight", "Commander", "Alchemist", "Arcanist", NULL }};
+    static const int NUM_RACES = 24;
+    static const int NUM_STATS = 6;       // NUM_STATS is used instead of NUM_CLASSES b/c class depends only on primary stats of a character
+    static const int NUM_VARIANTS = 3;
+    static const int NUM_UNIFORMS = 24;
+    static const int NUM_AUGMENTS = 24;
+    static const std::string races[NUM_RACES] = { "Wanderer", "Royalite", "Centurion", "Illuminate", "Shlaami", "Fishfolk", "Gekko",
+                                                  "Nomad", "Deathrazor", "Hiveling", "Ancient", "Lightsworn", "Drifter", "Goblin",
+                                                  "Swampfolk", "Tiki", "Titan", "Trogon", "Scaled", "Florbgon", "Oompa", "Wizened",
+                                                  "Necro", "Golem" };
+    static const std::string classes[NUM_STATS][NUM_STATS] = { { "", "Enforcer", "Gunner", "Machinist", "Darkmage", "Aethermage" },
+                                                               { "Enforcer", "", "Blademaster", "Dragoon", "Spellblade", "Aetherknight" },
+                                                               { "Gunner", "Blademaster", "", "Bounty Hunter", "Gunmage", "Commander" },
+                                                               { "Machinist", "Dragoon", "Bounty Hunter", "", "Datamancer", "Alchemist" },
+                                                               { "Darkmage", "Spellblade", "Gunmage", "Datamancer", "", "Arcanist" },
+                                                               { "Aethermage", "Aetherknight", "Commander", "Alchemist", "Arcanist", "" }};
+    static const std::string variants[NUM_VARIANTS] = { "Variant 1", "Variant 2", "Variant 3" };
+    static const std::string uniforms[NUM_UNIFORMS] = { "Fleet Cadet", "Hero", "Scholar", "Explorer", "Pyromancer", "Fairy", "Seer",
+                                                        "Soldier", "Blacksmith", "President", "Gadget Worker", "Minister", "Antihero",
+                                                        "Dirtmage", "Beehive", "Monster Trainer", "Scientist", "Crusader", "Echo",
+                                                        "Metalgear", "Phoenix", "Cobalt Mage", "Peasant", "Overworld" };
+    static const std::string augments[NUM_AUGMENTS] = { "Crusader Hat", "Rogue Bandana", "Berserker Scarf", "Mage Hat", "Crown", "Shmoo Hat",
+                                                        "Glibglob Hat", "Beats by Boizu", "Eyepod Hat", "Slime Hat", "Slime Hat",
+                                                        "Mech City Beanie", "Lucky Pumpkin", "Eye Gadget", "Baby Silver", "Oculus Goggles",
+                                                        "Chamcham Hat", "Demon Horns", "Forsaker Mask", "Shroom Hat", "Halo", "Creator Mask",
+                                                        "Rebellion Headpiece", "Gas Mask" };
 }
 #endif
