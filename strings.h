bdaed4e00a774c894ce00e41d8a409083231d357
Murphy Angelo <murphycangelo@gmail.com>
2016-06-28 16:45:46 -0400

    Add arrays for iteration and improve ease of reading

--- strings.h
+++ strings.h
@@ -9,7 +9,7 @@
  * to go through and change everything if the Roguelands save format changes slightly. */
 namespace Strings
 {
-    // The location of the save file is different on Windows and Mac
+    /*** Save Location ***/
 #if defined(Q_OS_WIN)
     static const std::string playerDataPrefix = "C:/Users/";
     static const std::string playerDataSuffix = "/AppData/LocalLow/DefaultCompany/Roguelands/PlayerPrefs.txt";
@@ -20,18 +20,28 @@ namespace Strings
 
     static const QString appTitle = "RogueEdit";
 
-    // Strings for UI
+    /*** User Interface ***/
+
+    /* Toolbar and Navigation */
     static const QString toolbarObjectName = "mainToolBar";
     static const QString navigationObjectName = "navigationWidget";
     static const QString characterMenuObjectName = "menuCharacter";
     static const QString loadCharacterDropdownObjectName = "dropdownLoad_Character";
+    static const QString saveCharacterActionText = "Save Character";
+
+    /* Character Tab */
+
     static const QString nameEditObjectName = "lineEditName";
+
+    // comboBoxes
     static const QString difficultyEditObjectName = "comboBoxDifficultyEdit";
     static const QString raceEditObjectName = "comboBoxRaceEdit";
     static const QString classEditObjectName = "comboBoxClassEdit";
     static const QString variantEditObjectName = "comboBoxVariantEdit";
     static const QString uniformEditObjectName = "comboBoxUniformEdit";
     static const QString augmentEditObjectName = "comboBoxAugmentEdit";
+
+    // spinBoxes
     static const QString vitalityEditObjectName = "spinBoxVitVal";
     static const QString dexterityEditObjectName = "spinBoxDexVal";
     static const QString magicEditObjectName = "spinBoxMagVal";
@@ -40,22 +50,22 @@ namespace Strings
     static const QString faithEditObjectName = "spinBoxFaiVal";
     static const QString characterLevelObjectName = "spinBoxLevelVal";
     static const QString allegianceLevelObjectName = "spinBoxAllegianceLevelVal";
-    static const QString saveCharacterActionText = "Save Character";
 
-    /* Strings for parsing PlayerPrefs.txt.
-     * Current representation: "specifier : value : type; " */
-    static const std::string separator = ":";   // Seperates information for a specifier
-    static const std::string terminator = ";";  // Terminates a specifier
+    /*** Parsing  ***/
+
+    //Current representation: "specifier : value : type; "
+    static const std::string separator = ":";              // Seperates information for a specifier
+    static const std::string terminator = ";";             // Terminates a specifier
     static const std::string paddedSeperator = " : ";
     static const std::string paddedTerminator = " ; ";
 
-    // (Soon to be) Long list of specifiers
+    /*** Specifiers ***/
     static const std::string intSpecifier = "System.Int32";
     static const std::string stringSpecifier = "System.String";
     static const std::string nameSpecifier = "name";
     static const std::string difficultySpecifier = "prof";
     static const std::string raceSpecifier = "race";
-    static const std::string classSpecifier = "class";     // WARNING: Does not have an associated value in PlayerPrefs.txt
+    static const std::string classSpecifier = "class";
     static const std::string primaryStat1 = "trait0";
     static const std::string primaryStat2 = "trait1";
     static const std::string variantSpecifier = "variant";
@@ -71,34 +81,75 @@ namespace Strings
     static const std::string allegianceLevelSpecifier = "aq";
 
 
-    // String arrays for determining settings
+    /*** Stats and Settings */
+
+    // Constant integers used to determine array/map sizes
     static const int NUM_DIFFICULTIES = 2;
     static const int NUM_RACES = 24;
-    static const int NUM_STATS = 6;       // NUM_STATS is used instead of NUM_CLASSES b/c class depends only on primary stats of a character
+    static const int NUM_STATS = 6;
+    static const int NUM_CLASSES = 15;
     static const int NUM_VARIANTS = 3;
     static const int NUM_UNIFORMS = 24;
-    static const int NUM_AUGMENTS = 24;
+    static const int NUM_AUGMENTS = 25;
+
+    /* Difficulty */
     static const std::string difficulties[NUM_DIFFICULTIES] = { "Normal", "Ironman"};
+
+    /* Races */
     static const std::string races[NUM_RACES] = { "Wanderer", "Royalite", "Centurion", "Illuminate", "Shlaami", "Fishfolk", "Gekko",
                                                   "Nomad", "Deathrazor", "Hiveling", "Ancient", "Lightsworn", "Drifter", "Goblin",
                                                   "Swampfolk", "Tiki", "Titan", "Trogon", "Scaled", "Florbgon", "Oompa", "Wizened",
                                                   "Necro", "Golem" };
-    static const std::string classes[NUM_STATS][NUM_STATS] = { { "", "Enforcer", "Gunner", "Machinist", "Darkmage", "Aethermage" },
-                                                               { "Enforcer", "", "Blademaster", "Dragoon", "Spellblade", "Aetherknight" },
-                                                               { "Gunner", "Blademaster", "", "Bounty Hunter", "Gunmage", "Commander" },
-                                                               { "Machinist", "Dragoon", "Bounty Hunter", "", "Datamancer", "Alchemist" },
-                                                               { "Darkmage", "Spellblade", "Gunmage", "Datamancer", "", "Arcanist" },
-                                                               { "Aethermage", "Aetherknight", "Commander", "Alchemist", "Arcanist", "" }};
+    /* Classes */
+    // Array for retrieving class name by ID
+    static const std::string classes[NUM_CLASSES] = { "Enforcer", "Gunner", "Machinist", "Darkmage", "Aethermage", "Blademaster", "Dragoon",
+                                                      "Spellblade", "Aetherknight", "Bounty Hunter", "Gunmage", "Commander", "Datamancer",
+                                                      "Alchemist", "Arcanist" };
+    // Array for finding class name by primary stats
+    static const std::string statCombos[NUM_STATS][NUM_STATS] = { { "", "Enforcer", "Gunner", "Machinist", "Darkmage", "Aethermage" },
+                                                                  { "Enforcer", "", "Blademaster", "Dragoon", "Spellblade", "Aetherknight" },
+                                                                  { "Gunner", "Blademaster", "", "Bounty Hunter", "Gunmage", "Commander" },
+                                                                  { "Machinist", "Dragoon", "Bounty Hunter", "", "Datamancer", "Alchemist" },
+                                                                  { "Darkmage", "Spellblade", "Gunmage", "Datamancer", "", "Arcanist" },
+                                                                  { "Aethermage", "Aetherknight", "Commander", "Alchemist", "Arcanist", "" }};
+
+    /* Variants */
     static const std::string variants[NUM_VARIANTS] = { "Variant 1", "Variant 2", "Variant 3" };
+
+    /* Uniforms */
     static const std::string uniforms[NUM_UNIFORMS] = { "Fleet Cadet", "Hero", "Scholar", "Explorer", "Pyromancer", "Fairy", "Seer",
                                                         "Soldier", "Blacksmith", "President", "Gadget Worker", "Minister", "Antihero",
                                                         "Dirtmage", "Beehive", "Monster Trainer", "Scientist", "Crusader", "Echo",
                                                         "Metalgear", "Phoenix", "Cobalt Mage", "Peasant", "Overworld" };
-    static const std::string augments[NUM_AUGMENTS] = { "Crusader Hat", "Rogue Bandana", "Berserker Scarf", "Mage Hat", "Crown", "Shmoo Hat",
+
+    /* Augments */
+    static const std::string augments[NUM_AUGMENTS] = { "", "Crusader Hat", "Rogue Bandana", "Berserker Scarf", "Mage Hat", "Crown", "Shmoo Hat",
                                                         "Glibglob Hat", "Beats by Boizu", "Eyepod Hat", "Slime Hat", "Slime Hat",
                                                         "Mech City Beanie", "Lucky Pumpkin", "Eye Gadget", "Baby Silver", "Oculus Goggles",
                                                         "Chamcham Hat", "Demon Horns", "Forsaker Mask", "Shroom Hat", "Halo", "Creator Mask",
                                                         "Rebellion Headpiece", "Gas Mask" };
+
+    /*** Arrays for Iteration ***/
+
+    // Total number of spinBoxes and comboBoxes on the character tab
+    static const int NUM_SPINBOXES = 8;
+    static const int NUM_COMBOBOXES = 6;
+
+    // Arrays for use in spinBox iteration
+    static const QString spinBoxObjectNames[NUM_SPINBOXES] = { vitalityEditObjectName, dexterityEditObjectName, magicEditObjectName,
+                                                               strengthEditObjectName, techEditObjectName, faithEditObjectName,
+                                                               characterLevelObjectName, allegianceLevelObjectName };
+    static const std::string spinBoxSpecifiers[NUM_SPINBOXES] = { vitalitySpecifier, dexteritySpecifier, magicSpecifier,
+                                                                  strengthSpecifier, techSpecifier, faithSpecifier,
+                                                                  characterLevelSpecifier, allegianceLevelSpecifier};
+
+    // Arrays for use in comboBox iteration
+    static const QString comboBoxObjectNames[NUM_COMBOBOXES] = { difficultyEditObjectName, raceEditObjectName, classEditObjectName,
+                                                                 variantEditObjectName, uniformEditObjectName, augmentEditObjectName };
+    static const std::string* comboBoxArrays[NUM_COMBOBOXES] = { &difficulties[0], &races[0], &classes[0],
+                                                                 &variants[0], &uniforms[0], &augments[0] };
+    static const std::string comboBoxSpecifiers[NUM_COMBOBOXES] = { difficultySpecifier, raceSpecifier, classSpecifier,
+                                                                    variantSpecifier, uniformSpecifier, augmentSpecifier };
 }
 
 #endif
