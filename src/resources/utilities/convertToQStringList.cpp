26403f78779988ba55fda6ca5d17372c4e618c1e
Murfalo <murphycangelo@u.northwestern.edu>
2016-12-17 17:43:59 -0500

    Fix indentation in utilities

--- src/resources/utilities/convertToQStringList.cpp
+++ src/resources/utilities/convertToQStringList.cpp
@@ -7,39 +7,39 @@
 /* Converts Windows Format text file (exported from Excel) containing item ID's and names into an array. */
 int main(int argc, char* argv[])
 {
-	if (argc != 3)
-	{
-		std::cout << "Usage: ./convert <source> <destination>";
-		return -1;
-	}
+  if (argc != 3)
+  {
+    std::cout << "Usage: ./convert <source> <destination>";
+    return -1;
+  }
 
-	std::stringstream parser;
-	std::ifstream src(argv[1]);   // Source file location
-	std::ofstream dest(argv[2]);  // Destination file location
+  std::stringstream parser;
+  std::ifstream src(argv[1]);   // Source file location
+  std::ofstream dest(argv[2]);  // Destination file location
 
-	if (src.fail()) std::cout << "Invalid source file." << std::endl;
-	if (dest.fail()) std::cout << "Invalid destination file." << std::endl;
+  if (src.fail()) std::cout << "Invalid source file." << std::endl;
+  if (dest.fail()) std::cout << "Invalid destination file." << std::endl;
 
-	parser << src.rdbuf();
+  parser << src.rdbuf();
 
-    static const std::string prefix = "static const QStringList itemList = {";
-	static const std::string suffix = "};";
+  static const std::string prefix = "static const QStringList itemList = {";
+  static const std::string suffix = "};";
 
-	std::string word;
-	std::string* output = new std::string(prefix);
+  std::string word;
+  std::string* output = new std::string(prefix);
 
-	while (parser >> word)
-	{
-		std::getline(parser, word);        // Grab the rest of the line
-        word.erase(std::remove(word.begin(), word.end(), '\t'), word.end());
-        word.erase(std::remove(word.begin(), word.end(), '\n'), word.end());
-        word.erase(std::remove(word.begin(), word.end(), '\r'), word.end());
-        *(output) += "\"" + word + "\",";
-	}
+  while (parser >> word)
+  {
+    std::getline(parser, word);  // Grab the rest of the line
+    word.erase(std::remove(word.begin(), word.end(), '\t'), word.end());
+    word.erase(std::remove(word.begin(), word.end(), '\n'), word.end());
+    word.erase(std::remove(word.begin(), word.end(), '\r'), word.end());
+    *(output) += "\"" + word + "\",";
+  }
 
-    output->pop_back();
-    output->append(suffix);   // Append the suffix
-	dest << *(output);        // Store the output in dest
-	dest.close();
-	delete output;
+  output->pop_back();
+  output->append(suffix);   // Append the suffix
+  dest << *(output);        // Store the output in dest
+  dest.close();
+  delete output;
 }
--- src/resources/utilities/convertToUnorderedMap.cpp
+++ src/resources/utilities/convertToUnorderedMap.cpp
@@ -7,40 +7,40 @@
 /* Converts Windows Format text file (exported from Excel) containing item ID's and names into an array. */
 int main(int argc, char* argv[])
 {
-	if (argc != 3)
-	{
-		std::cout << "Usage: ./convert <source> <destination>";
-		return -1;
-	}
+  if (argc != 3)
+  {
+    std::cout << "Usage: ./convert <source> <destination>";
+    return -1;
+  }
 
-	std::stringstream parser;
-	std::ifstream src(argv[1]);   // Source file location
-	std::ofstream dest(argv[2]);  // Destination file location
+  std::stringstream parser;
+  std::ifstream src(argv[1]);   // Source file location
+  std::ofstream dest(argv[2]);  // Destination file location
 
-	if (src.fail()) std::cout << "Invalid source file." << std::endl;
-	if (dest.fail()) std::cout << "Invalid destination file." << std::endl;
+  if (src.fail()) std::cout << "Invalid source file." << std::endl;
+  if (dest.fail()) std::cout << "Invalid destination file." << std::endl;
 
-	parser << src.rdbuf();
+  parser << src.rdbuf();
 
-    static const std::string prefix = "static const std::unordered_map<int, std::string> items = {";
-	static const std::string suffix = "};";
+  static const std::string prefix = "static const std::unordered_map<int, std::string> items = {";
+  static const std::string suffix = "};";
 
-	std::string ID;
-    std::string name;
-	std::string* output = new std::string(prefix);
+  std::string ID;
+  std::string name;
+  std::string* output = new std::string(prefix);
 
-	while (parser >> ID)
-	{
-		std::getline(parser, name);        // Grab the rest of the line
-        name.erase(std::remove(name.begin(), name.end(), '\t'), name.end());
-        name.erase(std::remove(name.begin(), name.end(), '\n'), name.end());
-        name.erase(std::remove(name.begin(), name.end(), '\r'), name.end());
-        *(output) += "{" + ID + ",\"" + name + "\"},";
-	}
+  while (parser >> ID)
+  {
+    std::getline(parser, name); // Grab the rest of the line
+    name.erase(std::remove(name.begin(), name.end(), '\t'), name.end());
+    name.erase(std::remove(name.begin(), name.end(), '\n'), name.end());
+    name.erase(std::remove(name.begin(), name.end(), '\r'), name.end());
+    *(output) += "{" + ID + ",\"" + name + "\"},";
+  }
 
-    output->pop_back();       // Remove extra comma
-    output->append(suffix);   // Append the suffix
-	dest << *(output);        // Store the output in dest
-	dest.close();
-	delete output;
+  output->pop_back();       // Remove extra comma
+  output->append(suffix);   // Append the suffix
+  dest << *(output);        // Store the output in dest
+  dest.close();
+  delete output;
 }
